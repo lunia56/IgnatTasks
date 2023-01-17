a22ea56cab9628e67f85125fc9a5440e1c799a0d
Ilya Lunin <lunia56@mail.ru>
2023-01-17 11:54:10 +0300

    Nomework 9

--- src/p2-homeworks/h5-rrd-v6/pages/Junior.tsx
+++ src/p2-homeworks/h5-rrd-v6/pages/Junior.tsx
@@ -11,7 +11,7 @@ const Junior = () => {
             Junior
             <HW7/>
             <HW8/>
-            {/*<HW9/>*/}
+            <HW9/>
             {/*<HW10/>*/}
             {/*<HW11/>*/}
         </div>
