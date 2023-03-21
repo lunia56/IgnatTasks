1646c22a06a2aedda02de397f07649135e618ccc
Ilya Lunin <lunia56@mail.ru>
2023-03-21 14:25:08 +0300

    hw1

--- src/s2-homeworks/hw05/pages/JuniorPlus.tsx
+++ src/s2-homeworks/hw05/pages/JuniorPlus.tsx
@@ -10,12 +10,12 @@ import HW15 from "../../hw15/HW15";
 function JuniorPlus() {
     return (
         <div id={'hw5-page-junior-plus'}>
-            <HW10 />
-            <HW11 />
-            <HW12 />
-            <HW13 />
-            <HW14/>
-            <HW15/>
+            {/*<HW10 />*/}
+            {/*<HW11 />*/}
+            {/*<HW12 />*/}
+            {/*<HW13 />*/}
+            {/*<HW14/>*/}
+            {/*<HW15/>*/}
         </div>
     )
 }
